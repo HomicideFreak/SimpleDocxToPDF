7adca365bcec68a227044c703d8b2bc3600dcc06
Mateusz <140084516+HomicideFreak@users.noreply.github.com>
2024-04-13 22:26:09 +0200

    Add convert whole folder functionality

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-from docx2pdf import convert
 import customtkinter
 import threading
 import os
@@ -8,7 +7,7 @@ customtkinter.set_appearance_mode("Dark")
 customtkinter.set_default_color_theme("dark-blue")
 
 root = customtkinter.CTk()
-root.geometry("500x450")
+root.geometry("500x500")
 root.title("Simple Docx to PDF Converter")
 root.resizable(False, False)
 iconPath = os.getcwd()
@@ -16,36 +15,53 @@ root.iconbitmap(iconPath+"\\Resources\\icon.ico")
 
 inFilePath = None
 outFilePath = None
+inFolderFiles = []
 
 def conv(pathIn, pathOut, entry):
     word = comtypes.client.CreateObject("Word.Application")
-    doc = word.Documents.Open(os.path.abspath(pathIn))
-    fileName = os.path.splitext(os.path.basename(pathIn))[0]
     entryValue = entry.get()
-    if outFilePath != None and outFilePath != "":
-        if entryValue == "" or entryValue == None:
-            print("")
-            print(os.path.abspath(pathOut  + "/" + fileName + ".pdf"))
-            print("")
-            doc.SaveAs(os.path.abspath(pathOut  + "/" + fileName + ".pdf"), FileFormat=17)
+    if os.path.isfile(pathIn):
+        doc = word.Documents.Open(os.path.abspath(pathIn))
+        fileName = os.path.splitext(os.path.basename(pathIn))[0]
+        if outFilePath != None and outFilePath != "":
+            if entryValue == "" or entryValue == None:
+                print("")
+                print(os.path.abspath(pathOut  + "/" + fileName + ".pdf"))
+                print("")
+                doc.SaveAs(os.path.abspath(pathOut  + "/" + fileName + ".pdf"), FileFormat=17)
+            else:
+                print("")
+                print(os.path.abspath(pathOut  + "/" + entryValue + ".pdf"))
+                print("")
+                doc.SaveAs(os.path.abspath(pathOut  + "/" + entryValue + ".pdf"), FileFormat=17)
         else:
-            print("")
-            print(os.path.abspath(pathOut  + "/" + entryValue + ".pdf"))
-            print("")
-            doc.SaveAs(os.path.abspath(pathOut  + "/" + entryValue + ".pdf"), FileFormat=17)
+            pathOut = os.path.dirname(os.path.abspath(pathIn))
+            if entryValue == "" or entryValue == None:
+                print("")
+                print(os.path.abspath(pathOut + "/" + fileName + ".pdf"))
+                print("")
+                doc.SaveAs(os.path.abspath(pathOut + "/" + fileName + ".pdf"), FileFormat=17)
+            else:
+                print("")
+                print(os.path.abspath(pathOut + "/" + entryValue + ".pdf"))
+                print("")
+                doc.SaveAs(os.path.abspath(pathOut + "/" + entryValue + ".pdf"), FileFormat=17)
+        doc.Close()
     else:
-        pathOut = os.path.dirname(os.path.abspath(pathIn))
-        if entryValue == "" or entryValue == None:
-            print("")
-            print(os.path.abspath(pathOut + "/" + fileName + ".pdf"))
-            print("")
-            doc.SaveAs(os.path.abspath(pathOut + "/" + fileName + ".pdf"), FileFormat=17)
+        if outFilePath != None and outFilePath != "":
+            for file in inFolderFiles:
+                doc = word.Documents.Open(os.path.abspath(file))
+                fileName = os.path.splitext(os.path.basename(file))[0]
+                doc.SaveAs(os.path.abspath(pathOut  + "/" + fileName + ".pdf"), FileFormat=17)
+                doc.Close()
         else:
-            print("")
-            print(os.path.abspath(pathOut + "/" + entryValue + ".pdf"))
-            print("")
-            doc.SaveAs(os.path.abspath(pathOut + "/" + entryValue + ".pdf"), FileFormat=17)
-    doc.Close()
+            pathOut = os.path.abspath(pathIn)
+            print(pathOut)
+            for file in inFolderFiles:
+                doc = word.Documents.Open(os.path.abspath(file))
+                fileName = os.path.splitext(os.path.basename(file))[0]
+                doc.SaveAs(os.path.abspath(pathOut  + "/" + fileName + ".pdf"), FileFormat=17)
+                doc.Close()
     word.Quit()
         
 def openFileDialInFile(label1):
@@ -53,10 +69,18 @@ def openFileDialInFile(label1):
     inFilePath = customtkinter.filedialog.askopenfilename()
     label1.configure(text = "In File Path: " + inFilePath)
     
-def openFileDialOutFile(label2, entry1):
+def openFileDialOutFile(label2):
     global outFilePath, noNameOutFilePath
     outFilePath = customtkinter.filedialog.askdirectory()
     label2.configure(text = "Out File Path: " + outFilePath)
+    
+def openFileDialInFileFolder(label1):
+    global inFilePath
+    inFilePath = customtkinter.filedialog.askdirectory()
+    label1.configure(text = "In File Folder Path: " + inFilePath)
+    for file_name in os.listdir(inFilePath):
+        if os.path.isfile(os.path.join(inFilePath, file_name)):
+            inFolderFiles.append(os.path.join(inFilePath, file_name))
         
 
 frame = customtkinter.CTkFrame(master=root)
@@ -71,11 +95,14 @@ entry1.pack(pady=12, padx=10)
 button_file_in = customtkinter.CTkButton(master=frame, text="Choose In File Path", command=lambda: openFileDialInFile(label1))
 button_file_in.pack(pady=12, padx=10)
 
+button_file_in = customtkinter.CTkButton(master=frame, text="Choose File Folder", command=lambda: openFileDialInFileFolder(label1))
+button_file_in.pack(pady=12, padx=10)
+
 label1_Text = "In File Path: " + str(inFilePath)
 label1 = customtkinter.CTkLabel(master=frame, text=label1_Text)
 label1.pack(pady=12, padx=10)
 
-button_file_out = customtkinter.CTkButton(master=frame, text="Choose Out File Path", command=lambda: openFileDialOutFile(label2, entry1))
+button_file_out = customtkinter.CTkButton(master=frame, text="Choose Out File Path", command=lambda: openFileDialOutFile(label2))
 button_file_out.pack(pady=12, padx=10)
 
 label2_Text = "Out File Path: Default (Same as Input)"
